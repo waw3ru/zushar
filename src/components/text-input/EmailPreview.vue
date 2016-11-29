58df4af847ac15edfa449cbf08d4a18c82c2638e
John Waweru <wambugu.john.waweru@outlook.com>
2016-11-29 13:11:10 +0300

    finished email component, url component and phone component

---
+++ src/components/text-input/EmailPreview.vue
@@ -0,0 +1,38 @@
+
+<template>
+  <div class="ui secondary segment">
+    <h5 class="ui dividing header">Component Preview</h5>
+
+    <form class="ui form" novalidate>
+      
+      <div class="field" :class="{ 'required': properties.isMandatory }">
+        <label v-if="properties.label">{{properties.label}}</label>
+
+        <div class="ui fluid input">
+          <input type="text" :placeholder="properties.placeholder">
+        </div>
+
+        <p class="content" v-if="properties.instructions">
+          {{ properties.instructions }}
+        </p>
+
+      </div>
+
+    </form>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'emailInput',
+  props: {
+    properties: {
+      type: Object,
+      default: {}
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ src/components/text-input/UrlPropertiesEditor.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="ui segment">
+    <h5 class="ui grey dividing header">Component Properties Editor</h5>
+
+    <form class="ui form" novalidate @submit.prevent="saveProps(current)">
+      <div class="field">
+        <label>Enter the question</label>
+        <div class="ui fluid input">
+          <input type="text" v-model.lazy="current.label" 
+          placeholder="Enter label">
+        </div>
+      </div>
+
+      <div class="field">
+        <label>Enter the question's instruction if any exist</label>
+        <div class="ui fluid input">
+          <input type="text" v-model.lazy="current.instructions" 
+          placeholder="Enter instruction">
+        </div>
+      </div>
+
+      <div class="field">
+        <label>Enter any placeholder for the question input</label>
+        <div class="ui fluid input">
+          <input type="text" v-model.lazy="current.placeholder" 
+          placeholder="Enter placeholder">
+        </div>
+      </div>
+
+      <div class="field">
+        <div class="ui checkbox">
+          <input type="checkbox" tabindex="0" class="hidden" 
+          v-model.lazy="current.isMandatory" :value="true">
+          <label>Should the question be mandatory to answer ?</label>
+        </div>
+      </div>
+
+      <div class="field">
+        <div class="ui checkbox">
+          <input type="checkbox" tabindex="0" class="hidden" 
+          v-model.lazy="current.validate" :value="true">
+          <label>Should the validity of url (web address) be checked ?</label>
+        </div>
+      </div>
+
+      <div class="ui divider"></div>
+
+      <button class="ui large labeled icon button" tabindex="0" type="submit">
+        <i class="save icon"></i> Save the Properties 
+      </button>
+
+    </form>
+
+  </div>
+</template>
+
+<script>
+import 'semantic/dist/components/checkbox.js'
+import { textInput } from '../../vuex/questionTypes.js'
+
+export default {
+  name: 'urlPropertiesEditor',
+  props: {
+    save: {
+      type: Function,
+      required: true
+    }
+  },
+  data() {
+    return {
+      current: Object.assign({}, textInput.url)
+    }
+  },
+  mounted() {
+    $('.ui.checkbox').checkbox();
+  },
+  methods: {
+    saveProps(data) {
+      this.save({
+        type: 'url',
+        props: Object.assign({}, data)
+      }, 
+      function () {
+        /*
+        * after submitting properties return the props form to default
+        * */
+        this.current = Object.assign({}, textInput.url);
+      }.bind(this));
+    }
+  }
+}
+</script>
+
+<style></style>
