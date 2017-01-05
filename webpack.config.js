9a8ff4b53e298e4b037e5ed74aa596efa9b152dd
John Waweru <wambugu.john.waweru@outlook.com>
2017-01-05 15:07:42 +0300

    removed source-maps for production build

--- webpack.config.js
+++ webpack.config.js
@@ -63,11 +63,10 @@ module.exports = {
 };
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map';
+  module.exports.devtool = false;
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
       compress: {
         warnings: false
       }
