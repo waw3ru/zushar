c0395cf2271bc8ffe5326f2ecb7937ffd3968124
John Waweru <wambugu.john.waweru@outlook.com>
2017-01-20 14:59:38 +0300

    Added version number to zushar titlebar and fixed it to automatically show from package.json version field

--- src/components/TitleBar.vue
+++ src/components/TitleBar.vue
@@ -1,18 +1,20 @@
 
 <template>
   <div id="title-bar">
-    <div class="ui container">
+    <div class="ui fluid container">
       <div class="ui two column grid">
         <div class="column">
           <img class="ui fluid small image logo" src="./../assets/logos/zushar_1.png">
         </div>
-        <div class="right floated five wide column">
-          <router-link :to="{ name: 'helpSupport' }" tag="button" class="ui tiny primary basic button btn">
+        <div class="right floated two wide column">
+          <!--<router-link :to="{ name: 'helpSupport' }" tag="button" class="ui tiny primary basic button btn">
             <i class="help circle outline icon"></i> Help and Support
-          </router-link>
+          </router-link>-->
+          <!--
           <button class="ui tiny basic button btn">
             <i class="sign out icon"></i> Exit
-          </button>
+          </button>-->
+          <h4 id="version_id" class="ui header">version v{{ version }}</h4>
         </div>
       </div>
     </div>
@@ -21,7 +23,12 @@
 
 <script>
   export default {
-    name: 'titleBar'
+    name: 'titleBar',
+    data() {
+      return {
+        version: process.env.version
+      }
+    }
   }
 </script>
 
@@ -35,9 +42,14 @@
   }
   #title-bar .logo{
     margin-top: 5px;
+    margin-left: 100px;
   }
   #title-bar .btn{
     margin-top: 7px;
     margin-left: 10px;
+  }
+  #version_id{
+    color: rgba(239,108,0,0.2);
+    margin-top: 16px;
   }
 </style>
